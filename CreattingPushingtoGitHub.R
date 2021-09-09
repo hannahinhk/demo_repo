827669082563afe3e9598365d3c50260956dade3
Hannah Wilson <hannahinhk@gmail.com>
2021-09-09 10:48:33 -0600

    Creating / Pushing to GitHub instructions

---
+++ CreattingPushingtoGitHub.R
@@ -0,0 +1,12 @@
+## Creating/Pushing to GitHub:=========================================
+# In Console: 
+# usethis::create_github_token()
+# - will take me to github token page, create new token, last up to 90 days (or more)
+# - copy it
+
+# In Console:
+# gitcreds::gitcreds_set() ; push enter
+# paste copied token from github page
+
+# Push to Github (Green up arrow)
+# Should go through
